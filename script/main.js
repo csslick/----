ef9d722b8d1649cd32f67aa1024c03af20883778
csslick <digitone@naver.com>
2023-12-21 16:37:59 +0900

    모버일 메뉴 animation

--- script/main.js
+++ script/main.js
@@ -1,5 +1,4 @@
 // main.js
-
 function toggleMenu() {
     const nav = document.querySelector('.title-container header nav');
     nav.classList.add('show'); // nav 나타남
@@ -7,4 +6,13 @@ function toggleMenu() {
     // 닫기 버튼 나타남
     const btnClose = document.querySelector('#btn-close');
     btnClose.classList.add('show');
+}
+
+function closeMenu() {
+    const nav = document.querySelector('.title-container header nav');
+    nav.classList.remove('show'); // nav 사라짐
+
+    // 닫기 버튼 사라짐
+    const btnClose = document.querySelector('#btn-close');
+    btnClose.classList.remove('show');
 }
