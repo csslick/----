76292ad7b7e0afdb280c2625e6c6cfef8f2d880f
csslick <digitone@naver.com>
2023-12-22 14:45:22 +0900

    nav transition 잔상 제거

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
 // main.js
 function toggleMenu() {
     const nav = document.querySelector('.title-container header nav');
+
+    // trasition 효과 켜기
+    nav.style.transition = 'all 0.3s';
+
     nav.classList.add('show'); // nav 나타남
 
     // 닫기 버튼 나타남
@@ -10,6 +14,10 @@ function toggleMenu() {
 
 function closeMenu() {
     const nav = document.querySelector('.title-container header nav');
+
+    // trasition 효과 끄기
+    nav.style.transition = 'none';
+
     nav.classList.remove('show'); // nav 사라짐
 
     // 닫기 버튼 사라짐
